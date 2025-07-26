ae77551ed591d0f759f9b5c47e7bda930eecee25
VidyaBhushan <vsinha015@gmail.com>
2025-07-26 21:52:44 +0800

    Initial Movie Card Setup
    Initial Movie Card Setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import MovieCard from './components/MovieCard'
+
+function App() {
+
+
+  return (
+    <>
+      <MovieCard movie={{title: "Myra's Film", release_date: "2025"}}/>
+      
+    </>
+  )
+}
+
+export default App
