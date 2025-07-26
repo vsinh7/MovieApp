89047d519e2a980e8c12379f5a1d857431b3e952
VidyaBhushan <vsinha015@gmail.com>
2025-07-26 23:28:42 +0800

    Implementation of State for Search Bar
    Implementation of State for Search Bar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,37 +1,45 @@
+import { useState } from "react";
 import MovieCard from "../components/MovieCard";
 
-
 function Home() {
+  const [searchQuery, setSearchQuery] = useState("");
 
-    const movies = [
-      {id: 1, title: "Myra's Movie", release_date:"2021"},
-      {id: 2, title: "Vidya's Movie", release_date:"1990"},
-      {id: 3, title: "Ankita's Movie", release_date:"1992"},
-      {id: 4, title: "Matrix's Movie", release_date:"1999"}
+  const movies = [
+    { id: 1, title: "Myra's Movie", release_date: "2021" },
+    { id: 2, title: "Vidya's Movie", release_date: "1990" },
+    { id: 3, title: "Ankita's Movie", release_date: "1992" },
+    { id: 4, title: "Matrix's Movie", release_date: "1999" },
+  ];
 
-    ];
-
-    const handleSearch = () =>{
-        
-    }
+  const handleSearch = (e) => {
+    e.preventDefault();
+    alert(searchQuery);
+    setSearchQuery("");
+  };
 
   return (
     <div className="home">
-        <form onSubmit={handleSearch} className="search-form">
-            <input type="text" className="search-input" 
-            placeholder="Search for Movies..." />
+      <form onSubmit={handleSearch} className="search-form">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Search for Movies..."
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
 
-            <button className="search-button" type="submit">Search</button>
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
 
-        </form>
-
-        <div className="movies-grid">
-            {movies.map((movie) =>(
-            <MovieCard movie={movie} key={movies.id}/>
-            ))}
-            </div>  
+      <div className="movies-grid">
+        {movies.map((movie) => (
+          <MovieCard movie={movie} key={movies.id} />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
